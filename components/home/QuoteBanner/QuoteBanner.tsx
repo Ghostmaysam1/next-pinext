5d3abee9531422454de41ef22cde57c3ecf0567c
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-12-06 01:10:41 +0330

    style: enhance layout responsiveness and text alignment in various components

--- components/home/QuoteBanner/QuoteBanner.tsx
+++ components/home/QuoteBanner/QuoteBanner.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 function QuoteBanner() {
     return (
         <div className='h-[260px] flex justify-center md:justify-between items-center'>
-            <div className="relative w-[30%] h-[260]">
+            <div className="relative w-[30%] h-[260] hidden md:block">
                 <Image alt='' src='/left.svg' fill className=' hidden md:block' />
             </div>
-            <div className='flex flex-col items-center'>
+            <div className='flex flex-col items-center text-center'>
                 <h1>با تخصص ما</h1>
                 <h2 className='text-[#5E5E66]'>برند شما میدرخشد</h2>
             </div>
-            <div className="relative w-[30%] h-[260]">
+            <div className="relative w-[30%] h-[260] hidden md:block">
                 <Image alt='' src='right.svg' fill className='hidden md:block' />
             </div>
         </div>
